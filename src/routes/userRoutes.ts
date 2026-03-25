9609e4127852e02166db316280a8b8826997d58e
souzdc <souzdc.dev@gmail.com>
2026-03-25 18:12:10 -0300

    chore(actions) prettier configuration for git actions

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { createUserController } from "../controller/createUserController";
+import { listUserController } from "../controller/listUserController";
 
 const router = Router();
 
 router.post("/users", createUserController);
+router.get("/users", listUserController)
 
 export default router;
